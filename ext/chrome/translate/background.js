86eea37b1cc904cc8bd7b311d66dcd4bc704c39e
Vitaly <job4vad@gmail.com>
2012-09-17 19:32:42 +0300

    update for extension

--- ext/chrome/translate/background.js
+++ ext/chrome/translate/background.js
@@ -1,5 +1,25 @@
+/*
+	*TODO:
+		- rating for each words how offen translation for it was showed
+		- add other notifications
+*/
+
 console.debugMode = true;
 function log(){if(console.debugMode = true) console.log(arguments);}
+
+
+
+
+var ownOptions = 4, initialTime = 10000;
+// Conditionally initialize the options.
+if (!localStorage.isInitialized) {
+	localStorage._isActivated = true;   // The display activation.
+	localStorage._frequency = 1;        // The display frequency, in minutes.
+	localStorage._isInitialized = true; // The option initialization.
+	localStorage._isNewWindow = true;	//open translation in new window
+}
+
+/********************************** Context menu functionality */
 
 var contextProp  = {
 	title: "Translate en-ru",
@@ -8,79 +28,112 @@ var contextProp  = {
 		var url = "http://translate.google.ru/#en|ru|" + info.selectionText;
 
 		//create tab with translation
-		chrome.tabs.create({
-			url: url
-		}, function(tabObject){
-
-			var url = "http://translate.google.ru/#en|ru|" + info.selectionText;
-
-			var req = new XMLHttpRequest();
-			req.open("GET", "http://translate.google.ru/translate_a/t?client=t&text="+ info.selectionText +"&hl=ru&sl=en&tl=ru&ie=UTF-8&oe=UTF-8&multires=1&ssel=0&tsel=0&sc=1", true);
-			req.onreadystatechange = function(){
-				 if (req.readyState == 4) {
-					if (req.status == 200) {
-
-						var ar = req.responseText.split(',');
-						for(var i=0; i< ar.length; i++){
-							if(!ar[i]) ar[i] = '""';
-						};
-						//convert string to array
-						var data = JSON.parse(ar.join(','));
-
-						//create new item
-						var item = {};
-						item.en = info.selectionText;
-						try{
-							item.ru = data[0][0][0];
-							item.synonyms = data[1][0][1];
-						}catch(e){}
-						log(data, item);
-						//save word
-						saveToLocal(item);
-					};
-				}
-			};
-			req.send(null);
-
-			log("tabObject=", tabObject);
-		});
+		if(localStorage._isNewWindow == "true"){
+			chrome.tabs.create({
+				url: url
+			}, function(tabObject){
+				getData(info);
+			});
+		}else{
+			getData(info);
+		};
 	}
 };
 
-function saveToLocal(item){
-	log("item=", item);
-	var lS = window.localStorage;
-	lS.setItem(item.en, item.toString());
-}
-function random(max){var min=0;return Math.floor(Math.random() * (max - min + 1)) + min;}
+/* add item to context menu */
+chrome.contextMenus.create( contextProp, function(){})
 
-function getNotification(){
+/********************************** Notification functionality */
+
+function getData(info,undefined){
+	var req = new XMLHttpRequest();
+	req.open("GET", "http://translate.google.ru/translate_a/t?client=t&text="+ info.selectionText +"&hl=ru&sl=en&tl=ru&ie=UTF-8&oe=UTF-8&multires=1&ssel=0&tsel=0&sc=1", true);
+	req.onreadystatechange = function(){
+		 if (req.readyState == 4) {
+			if (req.status == 200) {
+
+				var ar = req.responseText.split(',');
+				for(var i=0; i< ar.length; i++){
+					if(!ar[i]) ar[i] = '""';
+				};
+				//convert string to array
+				var data = JSON.parse(ar.join(','));
+
+				//create new item
+				var item = {};
+				item.en = info.selectionText;
+				log("Catched Data =",data);
+				try{
+					item.ru = data[0][0][0];
+					item.synonyms = data[1][0][1];
+				}catch(e){}
+				log(data, item);
+				//save word
+				saveToLocal(item);
+			};
+		}
+	};
+	req.send(null);
+
+	if(typeof tabObject != undefined) log("tabObject=", tabObject);
+}
+
+function saveToLocal(item){
+	log("Before Save item=", item);
+	var lS = window.localStorage;
+	lS.setItem(item.en, JSON.stringify(item));
+}
+function random(max){var min=ownOptions;return Math.floor(Math.random() * (max - min + 1)) + min;}
+
+function getNotification(undefined){
+	if(localStorage._isActivated == "false") return;
+
+	var item = getItem();
+
+	var note = window.webkitNotifications.createNotification(
+		'16.png',		// The image.
+		item.en.toUpperCase(),		// The title.
+		"press on me to get the answer"			// The body.
+	);
+	//var note = window.webkitNotifications.createHTMLNotification('index.html');
+	note.show();
+	hideNotification(note);
+
+	//Get new one notification with some period
+	setTimeout(function(){
+		getNotification();
+	}, localStorage._frequency * initialTime);
+
+	//Get this Item with translation
+	note.onclick = function(){
+		note.cancel();
+
+		note = window.webkitNotifications.createNotification(
+			'16.png',		// The image.
+			item.en + "  -  " + item.ru,		// The title.
+			(typeof item.synonyms !== typeof undefined ? item.synonyms : "")			// The body.
+		);
+		note.show();
+		hideNotification(note);
+	}
+}
+
+//Hide notification
+function hideNotification(note){
+	setTimeout(function(){
+		note.cancel();
+	},7000);
+}
+
+function getItem(){
 	var num = random(window.localStorage.length - 1), x=0;
 	for(var i in window.localStorage){
 		if(num == x){
-			var item =  JSON.parse(window.localStorage[i]);
-			break;
+			var item = JSON.parse(window.localStorage[i]);
+			log("Get Item", item, "item num =", num, window.localStorage.length - ownOptions);
+			return item;
 		}
 		x++;
 	};
-	console.log(item, "item", num, window.localStorage.length);
-	var notification = window.webkitNotifications.createNotification(
-		'tt.png',                      // The image.
-		item.en, // The title.
-		'ololo'      // The body.
-	);
-	notification.show();
-	setTimeout(function(){
-		notification.cancel();
-		getNotification();
-	}, 3000);
-
-	notification.onclick = function(){
-		alert(1);
-	}
-	//return item;
 }
-//getNotification();
-
-/* add item to context menu */
-chrome.contextMenus.create( contextProp, function(){})
+getNotification();
---
+++ ext/chrome/translate/options.js
@@ -0,0 +1,93 @@
+function disable(isDeactivated) {
+	// The label color.
+	options.style.color = isDeactivated ? 'graytext' : 'black';
+
+	// The control manipulability.
+	options.frequency.disabled = isDeactivated;
+}
+
+window.addEventListener('load', function() {
+	// Initialize the option controls.
+	options.isActivated.checked = JSON.parse(localStorage._isActivated);
+
+	// The display activation.
+	options.frequency.value = localStorage._frequency;
+
+	// The display frequency, in minutes.
+	if (!options.isActivated.checked) { disable(true); }
+
+	// Set the display activation and frequency.
+	options.isActivated.onchange = function() {
+		localStorage._isActivated = options.isActivated.checked;
+		disable(!options.isActivated.checked);
+	};
+	options.isNewWindow.onchange = function() {
+		localStorage._isNewWindow = options.isNewWindow.checked;
+	};
+
+	options.frequency.onchange = function() {
+		localStorage._frequency = options.frequency.value;
+	};
+
+/* View Settings */
+	options.getSettings.onclick = function(){
+		getSettings();
+		return false;
+	}
+
+	function getSettings(){
+		var container = document.querySelector('#settings'), ls = localStorage;
+		var html = "<table>";
+		for(var i in ls){
+			html += "<tr><td title='delete' class='deleteItem' data-key='"+ i +"'>[X]</td><td><strong>"+ i +"</strong></td><td>"+ ls[i] +"</td></tr>";
+		};
+		html += "</table>";
+		container.innerHTML = html;
+		initDeleteButton();
+	}
+
+	function initDeleteButton(){
+		var items = document.querySelectorAll("td.deleteItem");
+		for(var i=0, len = items.length; i<len; i++) {
+			(function(i){
+				items[i].onclick = function(e){
+					e.preventDefault();
+					delete localStorage[items[i].getAttribute("data-key")];
+					getSettings();
+				}
+			})(i);
+		};
+	}
+
+/* Export/Import Settings */
+	options.exportSettings.onclick = function(){
+		var container = document.querySelector('#settings'), ls = localStorage;
+		var html = "<textarea>"+ JSON.stringify(localStorage) +"</textarea>";
+		container.innerHTML = html;
+		return false;
+	}
+
+	options.importSettings.onclick = function(e){
+		var container = document.querySelector('#settings'), ls = localStorage, txt = document.querySelector('textarea');
+		if(txt && txt.value != ""){
+			try{
+				var data = JSON.parse(txt.value);
+			}catch(e){alert("Wrong JSON settings")}
+			if(typeof data != 'undefined'){
+				for(var i in data){
+					ls[i] = data[i];
+				};
+			}
+		}
+		return false;
+	}
+
+
+});
+
+
+
+
+
+
+
